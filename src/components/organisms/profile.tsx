a8796c73ca654e06ec514ea5d2d753490be131db
uehara1107 <u.sumika1107@gmail.com>
2023-10-29 15:05:42 +0900

    [change]imgtest
    画像が表示されないバグ修正のためのテスト

--- src/components/organisms/profile.tsx
+++ src/components/organisms/profile.tsx
@@ -6,7 +6,7 @@ const Profile: FC = () => (
   <div>
     <Box className={profstyle.header} bg="#00E39F" w="100%" />
     <div className={profstyle.content}>
-      <img className={profstyle.icon} src="../public/assets/graypic.png" />
+      <img className={profstyle.icon} src="./assets/graypic.png" />
       <Box className={profstyle.profArea}>
         <p className={profstyle.name}>UEHARA Sumika</p>
         <p className={profstyle.birthday}>2002.11.07</p>
